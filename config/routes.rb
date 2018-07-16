3184118f716a2af3978a85e326c389512d856fcf
mizoguchi kenta <347lionz@gmail.com>
2018-07-17 07:38:13 +0900

    make CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'tasks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # トップページの変更
-  root to: 'tasks#index'
+  root 'tasks#index'
+  get "tasks/show" => "tasks#show"
+  get "tasks/new" => "tasks#new"
+  get "tasks/edit" => "tasks#edit"
+
 
 end
