eca25a4ff76b584f50324071dca845998d09d90e
mizoguchi kenta <347lionz@gmail.com>
2018-08-05 10:54:39 +0900

    make loginForm and layout adjustment

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,39 +1,42 @@
 <template>
     <v-app id="inspire">
-
-            <v-navigation-drawer fixed v-model="drawer" app>
-                <v-list class="pa-0">
-                    <v-list-tile avatar>
-                        <v-list-tile-avatar>
-                            <img src="https://randomuser.me/api/portraits/men/85.jpg">
-                        </v-list-tile-avatar>
-                        <v-list-tile-content>
-                            <v-list-tile-title>John Leider</v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </v-list>
-                <v-list class="pt-0" dense>
-                    <v-divider></v-divider>
-                    <v-list-tile v-for="item in items" :key="item.title" :to="item.link">
-                        <v-list-tile-action>
-                            <v-icon>{{ item.icon }}</v-icon>
-                        </v-list-tile-action>
-
-                        <v-list-tile-content>
-                            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </v-list>
-            </v-navigation-drawer>
+        <v-navigation-drawer fixed v-model="drawer" app>
+            <v-list class="pa-0">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
+                        <img src="https://randomuser.me/api/portraits/men/85.jpg">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                        <v-list-tile-title>John Leider</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+            <v-list class="pt-0" dense>
+                <v-divider></v-divider>
+                <v-list-tile v-for="item in items" :key="item.title" :to="item.link">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
 
       <v-toolbar dark color="primary" fixed app>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title class="white--text">Title</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
+          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+          <v-toolbar-title class="white--text">Title</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
           <v-icon>search</v-icon>
-        </v-btn>
+          </v-btn>
       </v-toolbar>
+      <v-content>
+          <v-container fluid fill-height>
+              <router-view></router-view>
+          </v-container>
+      </v-content>
     </v-app>
 </template>
 
--- app/javascript/components/login_form.vue
+++ app/javascript/components/login_form.vue
@@ -1,14 +1,79 @@
 <template>
-    <p>Login_form</p>
+    <v-layout justify-center>
+        <v-snackbar
+                v-model="snackbar"
+                absolute
+                top
+                right
+                color="success"
+        >
+            <span>ログインが完了しました</span>
+            <v-icon dark>check_circle</v-icon>
+        </v-snackbar>
+        <v-flex xs12 sm10 md8 lg6>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="メールアドレス"
+                    required
+                    loading
+                ></v-text-field>
+                <v-text-field
+                    v-model="password"
+                    :append-icon="showPass? 'visibility_off':'visibility'"
+                    :rules="passRules"
+                    :type="showPass? 'text':'password'"
+                    :counter="10"
+                    label="パスワード"
+                    @click:append="showPass = !showPass"
+                    required
+                ></v-text-field>
+
+                <v-btn :disabled="!valid" @click="submit">ログイン</v-btn>
+                <v-btn @click="clear">やり直す</v-btn>
+            </v-form>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
+    export default {
+        data: function () {
+            return {
+                email: "",
+                password: "",
+                valid: true,
+                showPass: false,
+                snackbar: false,
+                emailRules: [
+                    v => !!v || 'メールアドレスを入力してください',
+                    v => /.+@.+/.test(v) || '正しい入力をしてください'
+                ],
+                passRules: [
+                    v => !!v || 'パスワードを入力してください',
+                    v => (v && v.length <= 10) || 'パスワードは10文字以下です'
+                ]
+            }
+        },
+        methods: {
+            submit() {
+                if (this.$refs.form.validate()){
 
+                }
+                this.snackbar = true
+            },
+            clear() {
+                this.$refs.form.reset()
+            }
+        }
+    }
 </script>
 
 <style scoped>
     p {
-        font-size: 20em;
+        font-size: 2em;
         text-align: center;
     }
+
 </style>
